1313d386ada5e4c0639dc0163d281e43543359e1
Jacky0727 <343154853@qq.com>
2016-08-11 12:17:44 +0800

    CubeRotate

---
+++ Assets/Scripts/CubeRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeRotate : MonoBehaviour
+{
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        transform.Rotate(new Vector3(0, 1, 0));
+	}
+}
